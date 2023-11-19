96d000bfe56faf5472f9c32166ca02e3a0ad7da2
Koen Cuijpers <koencuijp@Koens-MacBook-Air.local>
2023-11-19 13:13:34 +0000

    Add static page using matches component

---
+++ src/components/Matches.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Container, Row, Col, Card, Badge, ListGroup } from 'react-bootstrap';
+
+const currentTeam = 'Sterrenwijk Zondag 6'
+
+// Sample data for upcoming matches and signed-up players
+const upcomingMatches = [
+    {
+        id: 1,
+        date: '2023-11-20',
+        homeMatch: true,
+        opponent: 'Rentable FC 1',
+        playersJoining: ['Sybren', 'Max', 'Mans', 'Bram', 'Igor'],
+        playersNotJoining: ['Thijs', 'Teeuwen', 'Koen', 'Puck', 'Sam'],
+        playersPending: ['Jony', 'Ivo']
+
+    },
+    {
+        id: 2,
+        date: '2023-11-27',
+        homeMatch: false,
+        opponent: 'Rivierwijkers Zondag 5',
+        playersJoining: ['Sybren', 'Max', 'Mans', 'Milty', 'Koen'],
+        playersNotJoining: ['Thijs', 'Teeuwen', 'Koen', 'Puck', 'Sam'],
+        playersPending: ['Jony', 'Ivo']
+
+    },
+    {
+        id: 3,
+        date: '2023-12-05',
+        homeMatch: true,
+        opponent: 'Nieuwegein Zondag 2',
+        playersJoining: ['Sybren', 'Max', 'Mans', 'Bram', 'John', 'Timo'],
+        playersNotJoining: ['Thijs', 'Teeuwen', 'Koen', 'Puck', 'Sam'],
+        playersPending: ['Jony', 'Ivo']
+
+    },
+];
+
+export default function () {
+    return (
+        <Container fluid>
+            <h1 className="mt-4 mb-4">Upcoming Matches</h1>
+            {upcomingMatches.map((match) => (
+                <Row className="mb-4">
+                    <Col key={match.id}>
+                        <Card className='text-center'>
+                            <Card.Body>
+                                <Card.Title>
+                                    <h2 className="d-inline-block">{match.opponent}</h2>&nbsp;&nbsp;
+                                    <Badge
+                                        className="rounded-pill align-top"
+                                        bg={match.homeMatch ? 'primary' : 'secondary'}>
+                                        {match.homeMatch ? 'Home' : 'Away'}
+                                    </Badge>
+                                </Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted"><h3>{match.date}</h3></Card.Subtitle>
+                                <Row>
+                                    <Col key={1} md={4}>
+                                        <Card.Subtitle className="mt-3 mb-2"><Badge bg={'primary'}>Joining</Badge></Card.Subtitle>
+                                        <ListGroup variant="flush">
+                                            {match.playersJoining.map((player, index) => (
+                                                <ListGroup.Item key={index}>{player}</ListGroup.Item>
+                                            ))}
+                                        </ListGroup>
+                                    </Col>
+                                    <Col key={2} md={4}>
+                                        <Card.Subtitle className="mt-3 mb-2"><Badge bg={'danger'}>Not joining</Badge></Card.Subtitle>
+                                        <ListGroup variant="flush">
+                                            {match.playersNotJoining.map((player, index) => (
+                                                <ListGroup.Item key={index}>{player}</ListGroup.Item>
+                                            ))}
+                                        </ListGroup>
+                                    </Col>
+                                    <Col key={3} md={4}>
+                                        <Card.Subtitle className="mt-3 mb-2"><Badge bg={'warning'}>Not responded</Badge></Card.Subtitle>
+                                        <ListGroup variant="flush">
+                                            {match.playersPending.map((player, index) => (
+                                                <ListGroup.Item key={index}>{player}</ListGroup.Item>
+                                            ))}
+                                        </ListGroup>
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            ))}
+        </Container>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MatchOverview from './components/Matches';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap's default CSS
+import './css-customization.scss'; // custom CSS
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MatchOverview />
   </React.StrictMode>
 );
 
